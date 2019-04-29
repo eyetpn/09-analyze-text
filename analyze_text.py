22c358c991195209f07b7cd3fcf23116ce715d5c
eyetpn <eyetpn@gmail.com>
2019-04-29 11:18:42 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,12 @@
 # Remember to reach out for help after your own due diligence
 
 def analyze_text(text):
-    # Your code here
+    total_char = len(text)
+    num_e = 0
+    mystr = "e"
+    for char in text:
+        if char == "e" or "E":
+            num_e +=1
+    percent = num_e/total_char *100
+    print ("The text contains", total_char,"alphabetic characters, of which", num_e,"(",percent, "%) are 'e'.")
+    return  
